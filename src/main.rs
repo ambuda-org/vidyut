f7cc856cb41e09cb115af8d9043a6b02a6cb6627
Arun Prasad <akprasad.public@gmail.com>
2022-10-08 21:38:21 -0700

    Store CSV results in map

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,21 @@
+use std::collections::HashMap;
 use std::error::Error;
 use std::process;
 
-fn read_sandhi_rules() -> Result<(), Box<dyn Error>> {
+fn read_sandhi_rules() -> Result<HashMap<String, (String, String)>, Box<dyn Error>> {
+    let mut rules = HashMap::new();
+
     let mut rdr = csv::ReaderBuilder::new().delimiter(b'\t').from_path("data/sandhi.tsv")?;
-    for result in rdr.records() {
-        let record = result?;
-        println!("{:?}", record);
+    for maybe_row in rdr.records() {
+        let row = maybe_row?;
+        let first = String::from(&row[0]);
+        let second = String::from(&row[1]);
+        let result = String::from(&row[2]);
+        rules.insert(result.clone(), (first.clone(), second.clone()));
+        rules.insert(result.clone().replace(" ", ""), (first.clone(), second.clone()));
     }
-    Ok(())
+    println!("{:?}", rules);
+    Ok(rules)
 }
 
 fn main() {
