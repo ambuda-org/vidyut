f01d184b038ea4fd153c9804a7b5049f04dd7462
Arun Prasad <akprasad.public@gmail.com>
2022-10-08 22:58:35 -0700

    Fix sandhi rule bug

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,27 @@
-use std::collections::HashMap;
+use multimap::MultiMap;
 use std::error::Error;
 use std::process;
 use std::cmp;
 
-fn read_sandhi_rules() -> Result<HashMap<String, (String, String)>, Box<dyn Error>> {
-    let mut rules = HashMap::new();
+fn read_sandhi_rules() -> Result<MultiMap<String, (String, String)>, Box<dyn Error>> {
+    let mut rules = MultiMap::new();
 
     let mut rdr = csv::ReaderBuilder::new().delimiter(b'\t').from_path("data/sandhi.tsv")?;
     for maybe_row in rdr.records() {
         let row = maybe_row?;
         let first = String::from(&row[0]);
         let second = String::from(&row[1]);
-        let result = String::from(&row[2]);
+
+        let mut result = String::from(&row[2]);
         rules.insert(result.clone(), (first.clone(), second.clone()));
-        rules.insert(result.clone().replace(" ", ""), (first.clone(), second.clone()));
+
+        result = String::from(&row[2]).replace(" ", "");
+        rules.insert(result.clone(), (first.clone(), second.clone()));
     }
     Ok(rules)
 }
 
-fn split(input: &str, rules: HashMap<String, (String, String)>) {
+fn split(input: &str, rules: MultiMap<String, (String, String)>) {
     let len_longest_key = rules.keys().map(|x| x.len()).max().expect("Map is empty");
 
     let len_input = input.len();
@@ -28,12 +31,13 @@ fn split(input: &str, rules: HashMap<String, (String, String)>) {
 
         for j in i..cmp::min(len_input, i + len_longest_key) {
             let combination = &input[i..j];
-            match rules.get(combination) {
-                Some(split) => {
-                    let (f, s) = split;
-                    let first = String::from(&input[0..i]) + f;
-                    let second = String::from(s) + &input[j..len_input];
-                    println!("{}, {}", first, second);
+            match rules.get_vec(combination) {
+                Some(pairs) => {
+                    for (f, s) in pairs {
+                        let first = String::from(&input[0..i]) + f;
+                        let second = String::from(s) + &input[j..len_input];
+                        println!("{}, {}", first, second);
+                    }
                 },
                 None => continue
             }
@@ -43,7 +47,7 @@ fn split(input: &str, rules: HashMap<String, (String, String)>) {
 
 fn main() {
     match read_sandhi_rules() {
-        Ok(data) => split("Darmakzetre kurukzetre samavetA yuyutsavaH", data),
+        Ok(data) => split("Darmakzetre", data),
         Err(err) => {
             println!("{}", err);
             process::exit(1);
