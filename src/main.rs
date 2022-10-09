5119e4f83bf7b7f867075de2192ce8a298fe0dac
Arun Prasad <akprasad.public@gmail.com>
2022-10-09 07:49:44 -0700

    Accept CLI parameter

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,16 @@
 use multimap::MultiMap;
+use std::cmp;
 use std::error::Error;
 use std::process;
-use std::cmp;
 
 type SandhiMap = MultiMap<String, (String, String)>;
 
 fn read_sandhi_rules(tsv_path: &str) -> Result<SandhiMap, Box<dyn Error>> {
     let mut rules = MultiMap::new();
 
-    let mut rdr = csv::ReaderBuilder::new().delimiter(b'\t').from_path(tsv_path)?;
+    let mut rdr = csv::ReaderBuilder::new()
+        .delimiter(b'\t')
+        .from_path(tsv_path)?;
     for maybe_row in rdr.records() {
         let row = maybe_row?;
         let first = String::from(&row[0]);
@@ -19,18 +21,22 @@ fn read_sandhi_rules(tsv_path: &str) -> Result<SandhiMap, Box<dyn Error>> {
 
         let result_no_spaces = String::from(&row[2]).replace(" ", "");
         if result_no_spaces != result {
-          rules.insert(result_no_spaces, (first.clone(), second.clone()));
+            rules.insert(result_no_spaces, (first.clone(), second.clone()));
         }
     }
     Ok(rules)
 }
 
-fn split(input: &str, rules: SandhiMap) {
+fn split(input: &str, rules: SandhiMap) -> Vec<(String, String)> {
+    let mut res = Vec::new();
     let len_longest_key = rules.keys().map(|x| x.len()).max().expect("Map is empty");
     let len_input = input.len();
     for i in 0..len_input {
         // Default: split as-is, no sandhi.
-        println!("{}, {}", &input[0..i], &input[i..len_input]);
+        res.push((
+            String::from(&input[0..i]),
+            String::from(&input[i..len_input]),
+        ));
 
         for j in i..cmp::min(len_input, i + len_longest_key) {
             let combination = &input[i..j];
@@ -39,18 +45,25 @@ fn split(input: &str, rules: SandhiMap) {
                     for (f, s) in pairs {
                         let first = String::from(&input[0..i]) + f;
                         let second = String::from(s) + &input[j..len_input];
-                        println!("{}, {}", first, second);
+                        res.push((first, second))
                     }
-                },
-                None => continue
+                }
+                None => continue,
             }
         }
     }
+    res
 }
 
 fn main() {
+    let text = std::env::args().nth(1).expect("No text provided.");
+
     match read_sandhi_rules("data/sandhi.tsv") {
-        Ok(data) => split("Darmakzetre", data),
+        Ok(data) => {
+            for (first, second) in split(&text, data) {
+                println!("{} {}", first, second);
+            }
+        }
         Err(err) => {
             println!("{}", err);
             process::exit(1);
