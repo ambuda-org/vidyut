b0d705fbfc4c86cb507b6a61dfdec87b975fed20
Arun Prasad <akprasad.public@gmail.com>
2022-10-09 10:11:53 -0700

    Add unit test

--- src/main.rs
+++ src/main.rs
@@ -31,17 +31,22 @@ fn split(input: &str, rules: SandhiMap) -> Vec<(String, String)> {
     let mut res = Vec::new();
     let len_longest_key = rules.keys().map(|x| x.len()).max().expect("Map is empty");
     let len_input = input.len();
-    for i in 0..len_input {
+
+    // When iterating, prefer making the first item as long as possible, as longer
+    // items are easier to rule out.
+    for i in (1..=len_input).rev() {
         // Default: split as-is, no sandhi.
         res.push((
             String::from(&input[0..i]),
             String::from(&input[i..len_input]),
         ));
 
-        for j in i..cmp::min(len_input, i + len_longest_key) {
+        for j in i..cmp::min(len_input, i + len_longest_key + 1) {
             let combination = &input[i..j];
+            // println!("{}-{} : {}", i, j, combination);
             match rules.get_vec(combination) {
                 Some(pairs) => {
+                    println!("match");
                     for (f, s) in pairs {
                         let first = String::from(&input[0..i]) + f;
                         let second = String::from(s) + &input[j..len_input];
@@ -74,8 +79,22 @@ fn main() {
 #[cfg(test)]
 mod test {
     use super::*;
+
     #[test]
-    fn my_test() {
-        main()
+    fn basic_splits_work() {
+        let mut rules = SandhiMap::new();
+        rules.insert("e".to_string(), ("a".to_string(), "i".to_string()));
+        let expected: Vec<(String, String)> = vec![
+            ("ceti", ""),
+            ("cet", "i"),
+            ("ce", "ti"),
+            ("c", "eti"),
+            ("ca", "iti"),
+        ]
+        .iter()
+        .map(|&(f, s)| (f.to_string(), s.to_string()))
+        .collect();
+
+        assert_eq!(split("ceti", rules), expected);
     }
 }
