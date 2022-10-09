79862637154cbe2e93fd7ae4113a0a49184c7dbd
Arun Prasad <akprasad.public@gmail.com>
2022-10-08 21:19:13 -0700

    Add CSV reader for sandhi rules

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use std::error::Error;
+use std::process;
+
+fn read_sandhi_rules() -> Result<(), Box<dyn Error>> {
+    let mut rdr = csv::ReaderBuilder::new().delimiter(b'\t').from_path("data/sandhi.tsv")?;
+    for result in rdr.records() {
+        let record = result?;
+        println!("{:?}", record);
+    }
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    if let Err(err) = read_sandhi_rules() {
+        println!("{}", err);
+        process::exit(1);
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    fn my_test() {
+        main()
+    }
 }
