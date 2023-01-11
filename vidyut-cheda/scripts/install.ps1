53c466d37a247ffbfcf696c651d41c1ebb78daa1
Vikram Bhaskaran <vikram85.iyer@gmail.com>
2023-01-11 18:48:08 +1100

    [cheda] Add Powershell script for Windows
    * Updated env_logger to latest stable version

---
+++ vidyut-cheda/scripts/install.ps1
@@ -0,0 +1,31 @@
+Set-Location ..
+
+if (Test-Path "data") {
+	Remove-Item -LiteralPath data -Force -Recurse
+}
+
+if ((Test-Path "data-git") -eq $false) {
+	git clone --depth=1 https://github.com/sanskrit/data.git data-git
+}
+
+python data-git\bin\make_data.py --make_prefixed_verbals
+
+if (Test-Path "data-git") {
+	Copy-Item -Path "data-git/all-data" -Destination "data" -Force -Recurse 
+	Remove-Item -LiteralPath "data-git" -Force -Recurse
+}
+
+New-Item -Path "data/vidyut-0.1.0" -ItemType Directory
+
+Set-Variable RUST_LOG=info;
+
+make train 
+make create_kosha
+make generate_sandhi_rules
+
+cargo run --release --bin cheda -- "saMskftam" --data-dir "data/vidyut-0.1.0"
+
+Write-Output "                       "
+Write-Output "-----------------------"
+Write-Output "Vidyut is ready for use"
+Write-Output "------------------------"
