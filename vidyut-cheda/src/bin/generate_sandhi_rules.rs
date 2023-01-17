5dc7b980013b27a0266fae81d502c781ff45eadf
Arun Prasad <akprasad.public@gmail.com>
2023-01-16 22:07:06 -0800

    [cheda] Re-add `generate-sandhi-rules`
    I deleted this binary when I created `vidyut-sandhi`. This new version
    is much shorter than its predecessor and relies on library functions in
    `vidyut-sandhi` for the heavy lifting.

---
+++ vidyut-cheda/src/bin/generate_sandhi_rules.rs
@@ -0,0 +1,35 @@
+//! Generates most of the common sandhi rules that occur between two *pada*s.
+use clap::Parser;
+use std::error::Error;
+use std::path::{Path, PathBuf};
+use vidyut_cheda::Config;
+use vidyut_sandhi::{generate_rules, Rule};
+
+#[derive(Parser, Debug)]
+#[command(author, version, about)]
+struct Args {
+    /// Path to the output data directory.
+    #[arg(short, long)]
+    data_dir: PathBuf,
+}
+
+fn write_rules(rules: &[Rule], path: &Path) -> Result<(), Box<dyn Error>> {
+    let mut w = csv::Writer::from_path(path)?;
+    w.write_record(["first", "second", "result"])?;
+    for r in rules {
+        w.write_record([r.first(), r.second(), r.result()])?;
+    }
+    w.flush()?;
+    Ok(())
+}
+
+fn main() {
+    let args = Args::parse();
+    let rules = generate_rules();
+    let config = Config::new(&args.data_dir);
+
+    if let Err(err) = write_rules(&rules, config.sandhi()) {
+        println!("{}", err);
+        std::process::exit(1);
+    }
+}
