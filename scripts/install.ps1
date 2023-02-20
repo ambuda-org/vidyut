4c65d04f74e52207f6a0f06a08f8b90a9c362478
Vikram Bhaskaran <vikram85.iyer@gmail.com>
2023-02-21 06:24:00 +1100

    Fix the installation script on windows
    Have modified the installation script on windows to cater to vidyut
    0.2.0 structural changes

--- scripts/install.ps1
+++ scripts/install.ps1
@@ -1,29 +1,55 @@
-Set-Location ..
-
-if (Test-Path "data") {
-	Remove-Item -LiteralPath data -Force -Recurse
-}
-
-if ((Test-Path "data-git") -eq $false) {
-	git clone --depth=1 https://github.com/sanskrit/data.git data-git
-}
-
-python3 data-git\bin\make_data.py --make_prefixed_verbals
-
 if (Test-Path "data-git") {
-	Copy-Item -Path "data-git/all-data" -Destination "data" -Force -Recurse 
 	Remove-Item -LiteralPath "data-git" -Force -Recurse
 }
 
-New-Item -Path "data/vidyut-0.1.0" -ItemType Directory
+if (Test-Path "dcs-data") {
+	Remove-Item -LiteralPath "dcs-data" -Force -Recurse
+}
 
-make train 
+Set-Location ..
+
+Write-Output "------------------------"
+Write-Output "|    DCS Corpus Data   |"
+Write-Output "------------------------"
+
+if (Test-Path ".\data\raw\dcs") {
+	Write-Output "Training data already exists -- skipping fetch"
+}
+else {
+	Write-Output "Training data does not exist -- fetching"
+	New-Item -Path "data\raw\dcs" -ItemType Directory
+	git clone --depth 1 https://github.com/OliverHellwig/sanskrit.git dcs-data
+	Copy-Item -Path ".\dcs-data\dcs\data\conllu" -Destination ".\data\raw\dcs\conllu" -Force -Recurse
+	Remove-Item -LiteralPath "dcs-data" -Force -Recurse
+}
+
+Write-Output "-----------------------------"
+Write-Output "|   Linguistic Data Fetch   |"
+Write-Output "-----------------------------"
+
+if (Test-Path ".\data\raw\lex") {
+	Write-Output "Lexical data already exists -- skipping fetch"
+}
+else {
+	Write-Output "Lexical data does not exist -- fetching"
+	New-Item -Path ".\data\raw\lex" -ItemType Directory
+	git clone --depth=1 https://github.com/sanskrit/data.git data-git
+    python data-git/bin/make_data.py --make_prefixed_verbals
+	Copy-Item -Path ".\data-git\all-data\*" -Destination "data\raw\lex"
+	Remove-Item -LiteralPath "data-git" -Force -Recurse
+}
+
+Write-Output "-----------------------------"
+Write-Output "|        Vidyut Build       |"
+Write-Output "-----------------------------"
+
 make create_kosha
-make generate_sandhi_rules
-
-cargo run --release --bin cheda -- "saMskftam" --data-dir "data/vidyut-0.1.0"
+make test_kosha
+make create_sandhi_rules
+make train_cheda
+make eval_cheda
 
 Write-Output "                       "
-Write-Output "-----------------------"
-Write-Output "Vidyut is ready for use"
-Write-Output "------------------------"
+Write-Output "-------------------------"
+Write-Output " Vidyut is ready for use "
+Write-Output "-------------------------"
