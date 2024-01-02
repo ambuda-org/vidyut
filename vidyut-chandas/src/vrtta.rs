0cb21f97e1f3aa971e2418e0f6977bae5762e225
Malladi Pradyumna <f20210367@hyderabad.bits-pilani.ac.in>
2024-01-02 16:02:43 +0530

    Made the changes requested by @akprasad
    The non-trivial Changes:
    
    1. `to_weights` is now a TryFrom trait
    2. Removed the `weights_as_vector` method
    3. Removed the yati at the end of each pada from the data.

--- vidyut-chandas/src/vrtta.rs
+++ vidyut-chandas/src/vrtta.rs
@@ -74,31 +74,28 @@ impl Gana {
     }
 }
 
-fn to_weights(text: &str) -> Pada {
-    Pada {
-        weights: text.chars()
-        .filter_map(|c| match c {
-            '.' => Some(PatternWeight::Any),
-            'L' => Some(PatternWeight::L),
-            'G' => Some(PatternWeight::G),
-            _ => None,
-        })
-        .collect(),
-        yati: text.match_indices('|').enumerate().map(|(pos, (index, _))| index - pos).collect()
-    }
-}
-
 fn to_counts(text: &str) -> Vec<usize> {
     text.split_whitespace()
         .filter_map(|n| n.parse().ok())
         .collect()
 }
 
-/// Models a *feet/leg* of a verse, which constitutes of a specific pattern of light and heavy syllables along with some additional information like the yati positions.
+/// Models a *pāda*, which is one of the four "feet" or "legs" of a verse. 
+/// A *pāda* defines a specific pattern of light and heavy syllables and 
+/// might also define one or more *yati*s (caesuras).
 #[derive(Clone, Debug, Eq, Hash, PartialEq)]
 pub struct Pada {
     weights: Vec<PatternWeight>,
     yati: Vec<usize>
+}
+
+impl Pada {
+    fn new(weights: Vec<PatternWeight>, yati: Vec<usize>) -> Self {
+        Pada {
+            weights,
+            yati
+        }
+    }
 }
 
 /// Models a *vṛtta*, which defines a specific pattern of light and heavy syllables.
@@ -116,14 +113,6 @@ impl Vrtta {
             name: name.as_ref().to_string(),
             padas,
         }
-    }
-
-    // Just a small utility function to get the weights as a vector from the Vrtta struct
-    fn weights_as_vector(&self) -> Vec<Vec<PatternWeight>> {
-        self.padas.iter().fold(Vec::new(), |mut w, p| {
-            w.push(p.weights.clone());
-            w
-        })
     }
 
 
@@ -152,12 +141,12 @@ impl Vrtta {
         }
         eprintln!();
 
-        let w = self.weights_as_vector();
-
         let mut full = Vec::new();
         
         while full.len() < 4 {
-            full.extend(w.clone());
+            for p in &self.padas {
+              full.push(p.weights.clone());
+            }
         }
 
         debug_assert_eq!(full.len(), 4);
@@ -211,10 +200,12 @@ impl Vrtta {
         use PatternWeight::*;
 
         let mut result = Vec::new();
-        for pada in &self.weights_as_vector() {
+
+        
+        for pada in &self.padas {
             let mut ganas = Vec::new();
 
-            for chunk in pada.chunks(3) {
+            for chunk in pada.weights.chunks(3) {
                 match chunk {
                     [L, G, G] => ganas.push(Ya),
                     [G, G, G] => ganas.push(Ma),
@@ -241,6 +232,22 @@ impl Vrtta {
     }
 }
 
+impl TryFrom<&str> for Pada {
+    type Error = Box<dyn Error>;
+
+    fn try_from(text: &str) -> Result<Self, Self::Error> {
+        let weights: Vec<PatternWeight> = text.chars().filter_map(|c| match c {
+                '.' => Some(PatternWeight::Any),
+                'L' => Some(PatternWeight::L),
+                'G' => Some(PatternWeight::G),
+                _ => None,
+            })
+        .collect();
+        let yati: Vec<usize> = text.match_indices('|').enumerate().map(|(i, (offset, _))| offset - i).collect();
+        Ok(Pada::new(weights, yati))
+    }
+}
+
 impl TryFrom<&str> for Vrtta {
     type Error = Box<dyn Error>;
 
@@ -251,7 +258,8 @@ impl TryFrom<&str> for Vrtta {
         let name = fields[0];
         let _ = fields[1];
         let pattern_str = fields[2];
-        let padas: Vec<Pada> = pattern_str.split("/").map(to_weights).collect();
+        let padas: Result<Vec<Pada>, Box<dyn Error>> = pattern_str.split("/").map(|x| x.try_into()).collect();
+        let padas = padas?;
         Ok(Vrtta::new(name, padas))
     }
 }
